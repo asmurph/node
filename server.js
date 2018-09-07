27e63ee7795e7ff50239e32926ea2da61563b685
Murphy <Anthony.Murphy@nrtllc.com>
2018-09-06 21:09:02 -0500

    8

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-var express = require('express');  
-var app = express();  
+var express =require('express');
+//Initialize app with Express Web Framework
+var app = express();
+var _bodyParserPackage = require("body-parser")
+
 var cors = require("cors");
-var sql = require("mssql");
+//To parse result in json format  
+app.use(_bodyParserPackage.json());
 
-app.use(cors());
+//Here we will enable CORS, so that we can access api on cross domain.  
+app.use(function (req, res, next) {  
+    res.header("Access-Control-Allow-Origin", "*");  
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");  
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");  
+    next();  
+});  
 
-// config for your database
-var config = {
+
+//app.use(cors());
+var port=process.env.PORT || 1300;
+var _sqlPackage = require('mssql'); // MS Sql Server client
+
+app.use(express.static(__dirname + '/public'));
+var sqlconfig = {
     server: "mappers.database.windows.net", // Use your SQL server name
     database: "Appointments", // Database to connect to
     user: "smurph", // Use your username
@@ -18,27 +33,54 @@ var config = {
       }
    };
  
-app.get('/Regions', function (req, res) { 
- 
- // connect to your database
- sql.connect(config, function (err) {
- 
- if (err) console.log(err);
- 
- // create Request object
- var request = new sql.Request();
- 
- // query to the database and get the data
- request.query('SELECT * FROM dbo.Region', function (err, recordset) {
- 
- if (err) console.log(err)
- 
- // send data as a response
- res.send(recordset);
- 
- });
- });
+//Function to connect to database and execute query  
+var QueryToExecuteInDatabase = function (response, strQuery) {  
+    //close sql connection before creating an connection otherwise you will get an error if connection already exists.  
+    _sqlPackage.close();  
+    //Now connect your sql connection  
+    _sqlPackage.connect(sqlconfig, function (error) {  
+        if (error) {  
+            console.log("Error while connecting to database :- " + error);  
+            response.send(error);  
+        }  
+        else {  
+            //let's create a request for sql object  
+            var request = new _sqlPackage.Request();  
+            //Query to run in our database  
+            request.query(strQuery, function (error, responseResult) {  
+                if (error) {  
+                    console.log("Error while connecting to database:- " + error);  
+                    response.send(error);  
+                }  
+                else {  
+                    response.send(responseResult);  
+                }  
+            });  
+        }  
+    });             
+ }  
+  
+//GET API  
+app.get('/api/region', cors(), function(_req ,_res){  
+    var Sqlquery = "select * from region";  
+    QueryToExecuteInDatabase(_res, Sqlquery);  
+});  
+
+var regionRouter = express.Router();
+
+regionRouter.get('/', function(req, res){
+      var sqlQuerie = "select * from region"; 
+      QueryToExecuteInDatabase(res, sqlQuerie); 
 });
-var server = app.listen(4000, function () {
-    console.log('Server is running.. on Port 4000');
-   });
+
+app.use('/regions', regionRouter);
+
+
+app.get('/',function(req,res){
+console.log('hello from server');
+ res.render('./public/index.html');
+});
+
+app.listen(port);
+console.log('Server Listening at port'+port);
+module.exports = app;
